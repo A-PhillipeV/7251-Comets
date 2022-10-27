f6908cfd2ce0d9a3d68286b4fe945b304ba74789
itsalex125 <114694108+itsalex125@users.noreply.github.com>
2022-10-27 16:14:03 -0400

    Update RedAutoLeft.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Comp/RedAutoLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Comp/RedAutoLeft.java
@@ -1,89 +1,255 @@
 /*
- * MECANUM/HOLOMETRIC MODE TELE OP MODE
+ * Copyright (c) 2021 OpenFTC Team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 package org.firstinspires.ftc.teamcode.Comp;
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.Hardware.HardwareBudgetRobot;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
 
-@TeleOp(name="BudgetTeleOp", group="Comp")
-public class BudgetTeleOp extends LinearOpMode {
+import java.util.ArrayList;
 
+@Autonomous
+public class RedAutoLeft extends LinearOpMode
+{
+    OpenCvCamera camera;
+    AprilTagDetectionPipeline aprilTagDetectionPipeline;
     HardwareBudgetRobot robot = new HardwareBudgetRobot(this);
+
+    static final double FEET_PER_METER = 3.28084;
+
+    // Lens intrinsics
+    // UNITS ARE PIXELS
+    // NOTE: this calibration is for the C920 webcam at 800x448.
+    // You will need to do your own calibration for other configurations!
+    double fx = 578.272;
+    double fy = 578.272;
+    double cx = 402.145;
+    double cy = 221.506;
+
+    // UNITS ARE METERS
+    double tagsize = 0.166;
+
+     // Tag ID 1,2,3 from the 36h11 family
+    int tag1 = 1;
+    int tag2 = 2;
+    int tag3 = 3;
+
+    AprilTagDetection tagOfInterest = null;
+
     @Override
-    public void runOpMode() {
+    public void runOpMode()
+    {
+        //Robot and Dash
         robot.init();
         FtcDashboard dashboard = FtcDashboard.getInstance();
         telemetry = dashboard.getTelemetry();
 
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
+        //Camera
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
 
-        waitForStart();
-        while (opModeIsActive()) {
-            double x = -gamepad1.left_stick_y;
-            double y = -gamepad1.left_stick_x * 1.1;
-            double rx = gamepad1.right_stick_x;
-
-            boolean dYup = gamepad1.dpad_up;
-            boolean dYdown = gamepad1.dpad_down;
-
-            double gripPower = gamepad1.left_trigger; //servo
-
-            int currentPosition = robot.arm.getCurrentPosition();
-            //Servo to open/close hand
-            if(dYup)
-                robot.arm.setPower(0.5);
-            else if(dYdown)
-                robot.arm.setPower(-0.5);
-            else {
-                robot.arm.setPower(0);
-                robot.arm.setTargetPosition(currentPosition);
+        camera.setPipeline(aprilTagDetectionPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
             }
 
+            @Override
+            public void onError(int errorCode)
+            {
 
+            }
+        });
 
+        telemetry.setMsTransmissionInterval(50);
 
-            //Used to ensure same ratio and contain values between [-1,1]
+        while (!isStarted() && !isStopRequested())
+        {
+            ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
 
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = (y + x + rx) / denominator;
-            double backLeftPower = (y - x + rx) / denominator;
-            double frontRightPower = (y - x - rx) / denominator;
-            double backRightPower = (y + x - rx) / denominator;
+            if(currentDetections.size() != 0)
+            {
+                boolean tagFound = false;
 
+                for(AprilTagDetection tag : currentDetections)
+                {
+                    if(tag.id == tag1 || tag.id == tag2 || tag.id == tag3)
+                    {
+                        tagOfInterest = tag;
+                        tagFound = true;
+                        break;
+                    }
+                }
 
+                if(tagFound)
+                {
+                    telemetry.addLine("Tag of interest is in sight!\n\nLocation data:");
+                    tagToTelemetry(tagOfInterest);
+                }
+                else
+                {
+                    telemetry.addLine("Don't see tag of interest :(");
 
-            double throtte_control = .5;
-            double slowDown = 1;
-            if(gamepad1.right_bumper)
-                slowDown -= 0.25;
+                    if(tagOfInterest == null)
+                    {
+                        telemetry.addLine("(The tag has never been seen)");
+                    }
+                    else
+                    {
+                        telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                        tagToTelemetry(tagOfInterest);
+                    }
+                }
 
+            }
+            else
+            {
+                telemetry.addLine("Don't see tag of interest :(");
 
-            robot.motor1.setPower(frontLeftPower*throtte_control*slowDown);
-            robot.motor2.setPower(backLeftPower*throtte_control*slowDown);
-            robot.motor3.setPower(frontRightPower*throtte_control*slowDown);
-            robot.motor4.setPower(backRightPower*throtte_control*slowDown);
-            robot.hand.setPosition(gripPower);
+                if(tagOfInterest == null)
+                {
+                    telemetry.addLine("(The tag has never been seen)");
+                }
+                else
+                {
+                    telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                    tagToTelemetry(tagOfInterest);
+                }
 
-            telemetry.addData("frontLeft:", frontLeftPower);
-            telemetry.addData("backLeft:", backLeftPower);
-            telemetry.addData("frontRight:", frontRightPower);
-            telemetry.addData("backRight:", backRightPower);
+            }
+
             telemetry.update();
+            sleep(20);
+        }
+
+        /*
+         * The START command just came in: now work off the latest snapshot acquired
+         * during the init loop.
+         */
+
+        /* Update the telemetry */
+        if(tagOfInterest != null)
+        {
+            telemetry.addLine("Tag snapshot:\n");
+            tagToTelemetry(tagOfInterest);
+            telemetry.update();
+        }
+        else
+        {
+            telemetry.addLine("No tag snapshot available, it was never sighted during the init loop :(");
+            telemetry.update();
+        }
+
+        /* Actually do something useful */
+        if(tagOfInterest == null || tagOfInterest.id == tag1){
+            //trajectory
+            //Move Forward then Left
+
+            double x = -1;
+            double y = 1;
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x), 1);
+            double frontLeftPower = (y + x) / denominator;
+            double backLeftPower = (y - x) / denominator;
+            double frontRightPower = (y - x) / denominator;
+            double backRightPower = (y + x) / denominator;
+
+            double throtte_control = .75;
+
+            robot.motor1.setPower(frontLeftPower*throtte_control);
+            robot.motor2.setPower(backLeftPower*throtte_control);
+            robot.motor3.setPower(frontRightPower*throtte_control);
+            robot.motor4.setPower(backRightPower*throtte_control);
+
+            sleep(2000);
+
+            //Move Forward
+        }else if(tagOfInterest.id == tag2){
+            //trajectory
+            double x = 0;
+            double y = 1;
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x), 1);
+            double frontLeftPower = (y + x) / denominator;
+            double backLeftPower = (y - x) / denominator;
+            double frontRightPower = (y - x) / denominator;
+            double backRightPower = (y + x) / denominator;
+
+            double throtte_control = .75;
+
+            robot.motor1.setPower(frontLeftPower*throtte_control);
+            robot.motor2.setPower(backLeftPower*throtte_control);
+            robot.motor3.setPower(frontRightPower*throtte_control);
+            robot.motor4.setPower(backRightPower*throtte_control);
+
+            sleep(2000);
+
+        }else if(tagOfInterest.id == tag3){
+            //trajectory
+            //Move Forward then Left
+
+            double x = 1;
+            double y = 1;
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x), 1);
+            double frontLeftPower = (y + x) / denominator;
+            double backLeftPower = (y - x) / denominator;
+            double frontRightPower = (y - x) / denominator;
+            double backRightPower = (y + x) / denominator;
+
+            double throtte_control = .75;
+
+            robot.motor1.setPower(frontLeftPower*throtte_control);
+            robot.motor2.setPower(backLeftPower*throtte_control);
+            robot.motor3.setPower(frontRightPower*throtte_control);
+            robot.motor4.setPower(backRightPower*throtte_control);
+
+            sleep(2000);
 
         }
+
     }
 
-
-    /*void motorTelemetry() {
-        telemetry.addData("X", gamepad1.right_stick_x);
-        telemetry.addData("Front", gamepad1.right_stick_x);
-        telemetry.addData("Front", gamepad1.right_stick_x);
-    }*/
+    void tagToTelemetry(AprilTagDetection detection)
+    {
+        telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
+        telemetry.addLine(String.format("Translation X: %.2f feet", detection.pose.x*FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Y: %.2f feet", detection.pose.y*FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Z: %.2f feet", detection.pose.z*FEET_PER_METER));
+        telemetry.addLine(String.format("Rotation Yaw: %.2f degrees", Math.toDegrees(detection.pose.yaw)));
+        telemetry.addLine(String.format("Rotation Pitch: %.2f degrees", Math.toDegrees(detection.pose.pitch)));
+        telemetry.addLine(String.format("Rotation Roll: %.2f degrees", Math.toDegrees(detection.pose.roll)));
+    }
 }
-
