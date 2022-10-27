9eef75e1e34a781b92ddbf7e43e03f27f5446fa3
itsalex125 <114694108+itsalex125@users.noreply.github.com>
2022-10-27 16:11:24 -0400

    Update BudgetTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Comp/BudgetTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Comp/BudgetTeleOp.java
@@ -29,22 +29,46 @@ public class BudgetTeleOp extends LinearOpMode {
             double y = -gamepad1.left_stick_x * 1.1;
             double rx = gamepad1.right_stick_x;
 
+            boolean dYup = gamepad1.dpad_up;
+            boolean dYdown = gamepad1.dpad_down;
+
+            double gripPower = gamepad1.left_trigger; //servo
+
+            int currentPosition = robot.arm.getCurrentPosition();
+            //Servo to open/close hand
+            if(dYup)
+                robot.arm.setPower(0.5);
+            else if(dYdown)
+                robot.arm.setPower(-0.5);
+            else {
+                robot.arm.setPower(0);
+                robot.arm.setTargetPosition(currentPosition);
+            }
+
+
+
+
             //Used to ensure same ratio and contain values between [-1,1]
+
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
             double frontLeftPower = (y + x + rx) / denominator;
             double backLeftPower = (y - x + rx) / denominator;
             double frontRightPower = (y - x - rx) / denominator;
             double backRightPower = (y + x - rx) / denominator;
 
+
+
             double throtte_control = .5;
             double slowDown = 1;
             if(gamepad1.right_bumper)
                 slowDown -= 0.25;
 
+
             robot.motor1.setPower(frontLeftPower*throtte_control*slowDown);
             robot.motor2.setPower(backLeftPower*throtte_control*slowDown);
             robot.motor3.setPower(frontRightPower*throtte_control*slowDown);
             robot.motor4.setPower(backRightPower*throtte_control*slowDown);
+            robot.hand.setPosition(gripPower);
 
             telemetry.addData("frontLeft:", frontLeftPower);
             telemetry.addData("backLeft:", backLeftPower);
