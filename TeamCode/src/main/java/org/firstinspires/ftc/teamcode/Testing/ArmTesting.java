25420cc9bff87e7377416111324d79dfbb568117
A-PhillipeV <maano2446@gmail.com>
2022-12-08 18:03:50 -0500

    Arm Correcting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/ArmTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/ArmTesting.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.Testing;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -14,6 +15,7 @@ import org.firstinspires.ftc.teamcode.Hardware.HardwareBudgetRobot;
 public class ArmTesting extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
     DcMotor arm;
+    CRServo hand;
     final int lowHeight = 5800;
     final int middleHeight = 8400;
 
@@ -21,35 +23,47 @@ public class ArmTesting extends LinearOpMode {
     @Override
     public void runOpMode() {
         arm = hardwareMap.get(DcMotor.class, "Arm");
+        hand = hardwareMap.get(CRServo.class, "Hand");
         waitForStart();
 
-                double power = 1;
-                //If Y is pressed middle height
-                arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        double power = 1;
+        //If Y is pressed middle height
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-                while(opModeIsActive()) {
-                    if (gamepad1.y) {
-                        arm.setTargetPosition(middleHeight);
-                        arm.setPower(power);
-                        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        while(opModeIsActive()) {
+            if (gamepad1.y) {
+                arm.setTargetPosition(middleHeight);
+                arm.setPower(power);
+                arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-                    } else if (gamepad1.x) {
-                        arm.setTargetPosition(lowHeight);
-                        arm.setPower(power);
-                        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            } else if (gamepad1.x) {
+                arm.setTargetPosition(lowHeight);
+                arm.setPower(power);
+                arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-                    } else {
-                        arm.setTargetPosition(0);
-                        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    }
-
-
-                    telemetry.addData("Position", arm.getCurrentPosition());
-                    telemetry.update();
-                }
-
-
+            } else if (gamepad1.a){
+                arm.setTargetPosition(0);
+                arm.setPower(power);
+                arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             }
 
+            if(gamepad1.right_trigger > 0) {
+                hand.setPower(1);
+            }
+            else if(gamepad1.left_trigger < 0) {
+                hand.setPower(-1);
+            }
+            else {
+                hand.setPower(0);
+            }
+
+
+            telemetry.addData("Position", arm.getCurrentPosition());
+            telemetry.update();
+        }
+
+
     }
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/EncoderValueTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/EncoderValueTesting.java
@@ -1,28 +1,100 @@
 package org.firstinspires.ftc.teamcode.Testing;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+
 import org.firstinspires.ftc.teamcode.Hardware.HardwareBudgetRobot;
+
 @Config
-@TeleOp(name="EncoderValueTesting", group="Testing")
+@Autonomous(name="EncoderValueTesting", group="Testing")
 //@Disabled
 public class EncoderValueTesting extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
 
-    HardwareBudgetRobot robot = new HardwareBudgetRobot(this);
+    //HardwareBudgetRobot robot = new HardwareBudgetRobot(this);
+    DcMotor arm;
+    CRServo hand;
+
     @Override
     public void runOpMode() {
-        robot.init();
-
+        //robot.init();
+        arm = hardwareMap.get(DcMotor.class, "Arm");
+        hand = hardwareMap.get(CRServo.class, "Hand");
         waitForStart();
 
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        /* Write program to go to height 5400 (encoder tickers) and let it stay */
+        int lowHeight = 5800;
+        int maxHeight = 8400;
 
 
+        //Make the arm go up to low height go down to start and then go to middle height
+        // and then back down
+
+        //Go to low height
+        armMove(1, lowHeight, 0); //position = 5800
+        armMove(1, -1*lowHeight, 0); //position = 0
+
+        while (opModeIsActive()) {
+            telemetry.addData("Position", arm.getCurrentPosition());
+            telemetry.update();
+
+            //Kill switch
+            if(arm.getCurrentPosition() > 8500) {
+                arm.setPower(0);
+            }
+        }
+
+
+
+    }
+
+    public void armMove(double speed,
+                        int target,
+                        double timeoutS) {
+        int newMotor1Target;
+
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+
+            newMotor1Target = arm.getCurrentPosition() + target;
+
+            arm.setTargetPosition(newMotor1Target);
+
+            arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            arm.setPower(Math.abs(speed));
+
+
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (arm.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Running to", " %7d :%7d", newMotor1Target);
+                telemetry.addData("Currently at", " at %7d :%7d",
+                        arm.getCurrentPosition());
+                telemetry.update();
+            }
+
+            // Stop all motion;
+            arm.setPower(0);
+
+
+            // Turn off RUN_TO_POSITION
+            arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        }
     }
 }
