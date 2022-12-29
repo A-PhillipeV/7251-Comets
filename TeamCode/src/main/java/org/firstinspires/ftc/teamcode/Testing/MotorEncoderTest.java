8559e7c8458948fbf8c8dc23326bd2a63e655fc6
dvala041 <86671843+dvala041@users.noreply.github.com>
2022-12-29 14:57:20 -0500

    pre trial

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/MotorEncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/MotorEncoderTest.java
@@ -14,18 +14,36 @@ import org.firstinspires.ftc.teamcode.Hardware.HardwareBudgetRobot;
 //@Disabled
 public class MotorEncoderTest extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
-    DcMotor motor;
+    DcMotor motor1;
+    DcMotor motor2;
+    DcMotor motor3;
+    DcMotor motor4;
 
     @Override
     public void runOpMode() {
-        motor = hardwareMap.get(DcMotor.class, "Motor 1");
+        motor1 = hardwareMap.get(DcMotor.class, "Motor 1");
+        motor2 = hardwareMap.get(DcMotor.class, "Motor 2");
+        motor3 = hardwareMap.get(DcMotor.class, "Motor 3");
+        motor4 = hardwareMap.get(DcMotor.class, "Motor 4");
+
         waitForStart();
 
-        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor4.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        motor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor3.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor4.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         while(opModeIsActive()) {
-            telemetry.addData("Position", motor.getCurrentPosition());
+            telemetry.addData("Motor 1", motor1.getCurrentPosition());
+            telemetry.addData("Motor 2", motor2.getCurrentPosition());
+            telemetry.addData("Motor 3", motor3.getCurrentPosition());
+            telemetry.addData("Motor 4", motor4.getCurrentPosition());
+
             telemetry.update();
         }
     }
